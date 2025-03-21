e60b2563654a003bcc26be86e42704b19b67d59d
akradadiya28 <akradadiya28@gmail.com>
2025-03-21 19:47:55 +0530

    Notification sound functionality Added

--- views/src/hooks/useGetRealTimeMessage.jsx
+++ views/src/hooks/useGetRealTimeMessage.jsx
@@ -1,18 +1,27 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setMessages } from "../redux/messageSlice"
+import { playNotificationSound } from "../utils/audio"
 
 const useGetRealTimeMessage = () => {
 
     const { socket } = useSelector(store => store.socket)
     const { messages } = useSelector(store => store.message)
+    const { authUser } = useSelector(store => store.user)
     const dispatch = useDispatch();
 
     useEffect(() => {
         socket?.on('newMessage', (newMessage) => {
+            if (newMessage.senderId !== authUser?._id) {
+                playNotificationSound();
+            }
             dispatch(setMessages([...messages, newMessage]))
         })
-    }, [socket, setMessages, messages])
+
+        return () => {
+            socket?.off('newMessage');
+        }
+    }, [socket, setMessages, messages, authUser])
 }
 
 export default useGetRealTimeMessage
